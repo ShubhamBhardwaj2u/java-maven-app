194587968933b40b1d3b6d48b8d5fdaf517e5671
ShubhamBhardwaj2u <63233816+ShubhamBhardwaj2u@users.noreply.github.com>
2024-01-30 01:40:48 +0530

    Update buildPipelineScript.groovy

--- buildPipelineScript.groovy
+++ buildPipelineScript.groovy
@@ -1,9 +1,17 @@
 def buildApp(){
-  /
+  echo "Building the application"
+  sh "mvn package"
 }
 
 def buildANndDeploy(){
-  /
+          echo "Building image and deploying!"
+          withCredentials([
+            usernamePassword(credentialsId: 'azure cr', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')
+          ]){
+            sh "docker build -t privatecr.azurecr.io/application:1.2 ."
+            sh "echo $PASSWORD | docker login privatecr.azurecr.io -u $USERNAME --password-stdin"
+            sh "docker push privatecr.azurecr.io/application:1.2"
+          }
 }
 
 return this
