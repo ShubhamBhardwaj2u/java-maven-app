00f5534401b42efdf7b316fbc7a40f2b6fac7b2d
ShubhamBhardwaj2u <63233816+ShubhamBhardwaj2u@users.noreply.github.com>
2024-01-31 22:20:43 +0530

    Update scriptv100.groovy

--- scriptv100.groovy
+++ scriptv100.groovy
@@ -1,9 +1,13 @@
 def buildApp(){
-
+  echo "Building package"
+  sh "mvn package"
 }
-
+  
 def deployApp(){
-
+  echo "Deploying package"
+  sh "docker build -t privatecr.azurecr.io/app:1.0.0 ."
+  sh "echo $PASSWORD | docker login privatecr.azurecr.io -u $USERNAME --password-stdin"
+  sh "docker push privatecr.azurecr.io/app:1.0.0"
 }
 
 return this
