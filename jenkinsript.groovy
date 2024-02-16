ba25e5dd4226916e5d166e1a08dc87091f597782
ShubhamBhardwaj2u <63233816+ShubhamBhardwaj2u@users.noreply.github.com>
2024-02-16 20:50:34 +0530

    Update jenkinsript.groovy

--- jenkinsript.groovy
+++ jenkinsript.groovy
@@ -1,8 +1,10 @@
 def buildApp(){
   echo "Building app"
   echo "New version = ${NEW_VERSION}" // using environment variable
+  sh 'mvn -v' // using build tools
 }
 def testApp(){
+  // using credentials in jenkins
   withCredentials([
     usernamePassword(credentialsId: 'ShubhamBhardwaj2u', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')
   ]){
